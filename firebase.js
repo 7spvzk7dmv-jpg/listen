88adf2d642cee917ead1ff72e7542c331918a69b
7spvzk7dmv-jpg <7spvzk7dmv@privaterelay.appleid.com>
2025-12-15 14:42:46 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-import {
-  getAuth,
-  GoogleAuthProvider
-} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
 const firebaseConfig = {
